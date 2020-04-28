20f9ffa8afb8a6c0d3335347c22362232fe9fb59
mayursamnani36 <54547503+mayursamnani36@users.noreply.github.com>
2020-04-28 20:01:06 +0530

    made improvements in google auth

--- lib/services/google_auth.dart
+++ lib/services/google_auth.dart
@@ -1,31 +1,50 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class GoogleAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
   //sign in with Google
   Future<FirebaseUser> googleSignIn() async {
     try{
-      GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-      GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
-      final AuthCredential credential = GoogleAuthProvider.getCredential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-      );
-      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
-      return user;
+      GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn(); // this shows the popup screen
+      if(googleSignInAccount!=null){ // check if we go back without tapping on any account
+        GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+        final AuthCredential credential = GoogleAuthProvider.getCredential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+        final AuthResult result = (await _auth.signInWithCredential(credential));
+        final FirebaseUser user = result.user;
+        // print(user);
+        // print(result);
+        assert(user.email!=null);
+        assert(user.displayName != null);  
+        assert(!user.isAnonymous);
+        assert(await user.getIdToken() != null);
+        
+        final FirebaseUser currentUser = await _auth.currentUser();
+        assert(currentUser.uid == user.uid);
+        
+        return user;
       }
+      return null;
+      }
+    on PlatformException catch(e){
+      print(e.toString());
+      return null;
+    }
     catch(e){
-      print(e); return null;
+      print("other error");
+      print(e.toString());
+      return null;
       }
   }
 
   //logout
   void googleSignOut() async {
+    await _auth.signOut();
     await _googleSignIn.signOut();
     print("User signed out");
   }
-
-}
